4714fc3c6b292b9ebf8d086392170f002aca8094
Walter Souza da Silva Junior <wjuniorbh@outlook.com>
2018-10-21 05:18:24 -0300

    add attribute.service to public api

--- public_api.ts
+++ public_api.ts
@@ -1,3 +1,5 @@
 export * from './src/app/modules/dynamic/dynamic.module'
-export * from './src/app/modules/dynamic/model/dynamic-item-config'
 export * from './src/app/modules/dynamic/dynamic-item'
+export * from './src/app/modules/dynamic/service/attribute.service'
+export * from './src/app/modules/dynamic/model/dynamic-item-config'
+export * from './src/app/modules/dynamic/model/type.enum'
